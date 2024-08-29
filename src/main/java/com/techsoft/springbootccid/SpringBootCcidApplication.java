b91b1ef8cf2e1607f1d14f7cdcd3a122ac66ea3c
SanyamJain15 <sanyam.jain@techurate.com>
2024-08-29 15:41:25 +0530

    first commit

---
+++ src/main/java/com/techsoft/springbootccid/SpringBootCcidApplication.java
@@ -0,0 +1,13 @@
+package com.techsoft.springbootccid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCcidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCcidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techsoft/springbootccid/SpringBootCcidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsoft.springbootccid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCcidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
